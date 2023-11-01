45baca40517246d90028f26eb29e71e75a9f0d8c
Tejaswinibobba <tejaswinibobba@gmail.com>
2023-11-01 18:48:53 -0500

    bonus

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var usersRouter = require('./routes/users');
 var furnitureRouter = require('./routes/furniture');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-var boardRouter = require('./routes/board');
 
 var app = express();
 
@@ -27,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/furniture',furnitureRouter);
 app.use('/board',boardRouter);
-app.use('/board',boardRouter);
+app.use('/choose',chooseRouter);
 
 
 // catch 404 and forward to error handler
