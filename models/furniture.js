bf31fc0bfa9d9dafd2b82f3010f9bd41f0372709
Tejaswinibobba <tejaswinibobba@gmail.com>
2023-11-29 17:53:46 -0600

    changes added

--- models/furniture.js
+++ models/furniture.js
@@ -1,7 +1,27 @@
 const mongoose = require("mongoose")
 const furnitureSchema = mongoose.Schema({
-itemName: String,
-quantity: Number,
-cost: Number
+itemName:{
+        type:String,
+        required:true,
+        validate: {
+            validator: function (value) {
+                // You can customize this validation logic according to your requirements
+                return /^[a-zA-Z\s]+$/.test(value); // Example: Allow only letters and spaces
+            },
+            message: "Invalid itemName, please use only letters and spaces.",
+        },
+    },
+quantity:{
+        type:Number,
+        required:true,
+        min:1,
+        max:90
+    },
+cost:{
+     type:Number,
+     required:true,
+     min:10,
+     max:750
+} 
 })
 module.exports = mongoose.model("furniture",furnitureSchema)
--- routes/furniture.js
+++ routes/furniture.js
@@ -1,6 +1,13 @@
 var express = require('express');
 const furniture_controlers= require('../controllers/furniture');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    res.redirect("/login");
+    }
 /* GET furniture */
 router.get('/', furniture_controlers.furniture_view_all_Page);   
 /* GET detail furniture page */
@@ -12,8 +19,9 @@ module.exports = router;
 router.get('/update', furniture_controlers.furniture_update_Page);
 /* GET delete  page */
 router.get('/delete', furniture_controlers.furniture_delete_Page);
-
-
+/* GET update furniture page */
+router.get('/update', secured, furniture_controlers.furniture_update_Page);
+module.exports = router;
 
 
 
